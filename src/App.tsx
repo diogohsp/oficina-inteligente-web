592e11e2999dc18c06cc283173cb4f3e4ffaa6e1
hspDiogo <diogo.prado@gruponsc.com.br>
2024-06-05 14:30:07 -0300

    feat: adição zod e toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import "./global.css";
+import './global.css'
 
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
-import { HelmetProvider, Helmet } from "react-helmet-async";
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
+import { HelmetProvider, Helmet } from 'react-helmet-async'
+import { Toaster } from 'sonner'
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | Oficina.Inteligente" />
+      <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
-  );
+  )
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,8 +2,45 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { toast } from 'sonner'
+
+const signInForm = z.object({
+  email: z
+    .string()
+    .min(1, { message: 'O email é obrigatório' })
+    .email('O email inserido é invalido'),
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>({
+    resolver: zodResolver(signInForm),
+  })
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      console.log(data)
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      toast.success('Enviamos um link de autenticação para seu email.'),
+        {
+          action: {
+            label: 'Reenviar',
+            onClick: () => handleSignIn(data),
+          },
+        }
+    } catch (error) {
+      toast.error('Credenciais inválidas')
+    }
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -18,13 +55,13 @@ export function SignIn() {
             </p>
           </div>
 
-          <form action="" className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-4">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email"></Input>
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button disabled={isSubmitting} className="w-full" type="submit">
               Acessar painel
             </Button>
           </form>
