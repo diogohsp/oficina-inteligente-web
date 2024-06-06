00c993b64dcdba38f680c2afe4da312dc625da44
Diogo <ggdiogogg@gmail.com>
2024-06-06 00:22:35 -0300

    feat: adicionado formulario de registro

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -6,70 +6,79 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { toast } from 'sonner'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
-const signInForm = z.object({
+const signUpForm = z.object({
   email: z
     .string()
     .min(1, { message: 'O email é obrigatório' })
     .email('O email inserido é invalido'),
+  senha: z.string().min(3, 'A senha deve ter no minimo 3 caracteres'),
 })
 
-type SignInForm = z.infer<typeof signInForm>
+type SignUpForm = z.infer<typeof signUpForm>
 
-export function SignIn() {
+export function SignUp() {
+  const navigate = useNavigate()
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>({
-    resolver: zodResolver(signInForm),
+  } = useForm<SignUpForm>({
+    resolver: zodResolver(signUpForm),
   })
 
-  async function handleSignIn(data: SignInForm) {
+  async function handleSignUp(data: SignUpForm) {
     try {
       console.log(data)
       await new Promise((resolve) => setTimeout(resolve, 2000))
-      toast.success('Enviamos um link de autenticação para seu email.'),
-        {
-          action: {
-            label: 'Reenviar',
-            onClick: () => handleSignIn(data),
-          },
-        }
+      toast.success('Cadastro realizado com sucesso', {
+        action: {
+          label: 'login',
+          onClick: () => navigate('/sign-in'),
+        },
+      })
     } catch (error) {
-      toast.error('Credenciais inválidas')
+      toast.error('Erro ao cadastrar')
     }
   }
 
   return (
     <>
-      <Helmet title="Login" />
+      <Helmet title="Cadastro" />
       <div className="p-8">
         <Button variant="ghost" asChild className="absolute right-8 top-8">
-          <Link to="/sign-up">
-            Novo Cadastro
-          </Link>
+          <Link to="/sign-in">Fazer Login</Link>
         </Button>
         <div className="w[350px] flex flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
-              Acessar painel
+              Criar conta
             </h1>
             <p className="text-sm text-muted-foreground">
               Acompanhe seus clientes
             </p>
           </div>
 
-          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignUp)} className="space-y-4">
             <div className="space-y-4">
               <Label htmlFor="email">Seu e-mail</Label>
               <Input id="email" type="email" {...register('email')} />
             </div>
+            <div className="space-y-4">
+              <Label htmlFor="senha">Sua senha</Label>
+              <Input id="senha" type="password" {...register('senha')} />
+            </div>
 
             <Button disabled={isSubmitting} className="w-full" type="submit">
-              Acessar painel
+              Finalizar cadastro
             </Button>
+
+            <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
+              Ao continuar, você concorda com nosso termos de serviço e
+              políticas de privacidade.
+            </p>
           </form>
         </div>
       </div>
